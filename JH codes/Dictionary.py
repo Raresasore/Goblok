f40eb244f62ece46d62127c9b0bd01a581a5ee45
Raresasore <126657939+Raresasore@users.noreply.github.com>
2023-03-02 01:19:05 +0800

    Add files via upload

---
+++ JH codes/Dictionary.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#%% Dictionary 
+
+prop_list = []
+file = open("property-listing.txt", "r")
+for each in file:
+    prop_list.append(each.strip())
+file.close()
+
+listing_points= []
+    
+for attribute in prop_list:
+
+   listing_points
+       
+       
+print(prop_list)
